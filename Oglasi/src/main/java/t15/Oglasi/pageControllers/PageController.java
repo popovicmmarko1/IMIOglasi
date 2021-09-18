d39a8a74de7b0008d29c7614f7f2e5eebce8e75e
Marko Popovic <popovic.mmarko1@gmail.com>
2021-09-18 16:15:14 +0200

    wip

--- Oglasi/src/main/java/t15/Oglasi/pageControllers/PageController.java
+++ Oglasi/src/main/java/t15/Oglasi/pageControllers/PageController.java
@@ -303,18 +303,6 @@ public class PageController {
         }
     }
 
-    @GetMapping("/oglas/test")
-    public String test(Model model)
-    {
-        model.addAttribute("naslov", "Posao 1");
-        model.addAttribute("mesto", "Mesto 1");
-        model.addAttribute("postavljen", LocalDateTime.now().toString());
-        model.addAttribute("istice", LocalDateTime.now().plusDays(20).toString());
-        model.addAttribute("poslodavac", "Poslodavac 1");
-        model.addAttribute("opis", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
-        model.addAttribute("poslodavac_opis", "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
-        return "oglas";
-    }
 
     @GetMapping("/oglas/index")
     public String oglasindex(Model model)
